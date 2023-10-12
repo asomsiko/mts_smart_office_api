3b2e586e97910695a7fb02bd0b1989c968502ea6
muz <nadokto8@gmail.com>
2023-10-12 18:55:26 +1000

    routes + new models

--- app/routes.py
+++ app/routes.py
@@ -6,10 +6,10 @@ from flask import request
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import current_user
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token
 
+from werkzeug.security import generate_password_hash
 
-#TODO: сделать апи для регистрации
 
 @jwt.user_identity_loader
 def user_identity_lookup(user):
@@ -20,27 +20,42 @@ def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
     return User.query.filter_by(id=identity).one_or_none()
 
+@app.route('/signup', methods=['POST'])
+def signup_post():
 
-@app.route("/login", methods=["POST"])
+    email = request.json.get('email')
+    name = request.json.get('name')
+    surname = request.json.get('surname')
+    patronymic = request.json.get('patronymic')
+    phone = request.json.get('phone')
+    rating = request.json.get('rating')
+    state = request.json.get('state')
+    password = request.json.get('password_hash')
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({"error": "Email address already exists"}), 400
+
+    new_user = User(email=email, name=name, password_hash=generate_password_hash(password, method='pbkdf2:sha256'),
+        surname=surname, patronymic=patronymic, phone=phone, rating=rating, state=state)
+    access_token = create_access_token(identity=new_user)
+    refresh_token = create_refresh_token(identity=new_user)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User created", "access_token": access_token, "refresh_token": refresh_token}), 201
+
+@app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-
-    user = User.query.filter_by(username=username).one_or_none()
-    if not user or not user.check_password(password):
-        return jsonify("Wrong username or password"), 401
-
-    # Notice that we are passing in the actual sqlalchemy user object here
+    #TODO: функция которая сырой пароль сравнивает с хэшем
+    email = request.json.get('email')
+    password = request.json.get('password')
+    password = generate_password_hash(password, method='pbkdf2:sha256')
+    user = User.query.filter_by(email=email).first()
+    if not user or not User.check_password(password=password):
+        return jsonify({"error": "Bad login credentials"}), 401
+    
     access_token = create_access_token(identity=user)
-    return jsonify(access_token=access_token)
+    refresh_token = create_refresh_token(identity=user)
 
-
-@app.route("/who_am_i", methods=["GET"])
-@jwt_required()
-def protected():
-    # We can now access our sqlalchemy User object via `current_user`.
-    return jsonify(
-        id=current_user.id,
-        name=current_user.name,
-        surname=current_user.surname,
-    )
+    return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
