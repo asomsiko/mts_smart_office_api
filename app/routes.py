96e8799288b14d2e477ac378b3279e7035dadea7
muz <nadokto8@gmail.com>
2023-10-13 20:42:55 +1000

    feat: роуты для сервисов и нейронок

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import requests
+
 from app import app, db, jwt
 from app.models import User
 
@@ -48,7 +50,7 @@ def signup_post():
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": str(e)}), 500
-    
+
 @app.route('/login', methods=['POST'])
 def login():
     email = request.json.get('email')
@@ -64,3 +66,98 @@ def login():
     access_token = create_access_token(identity=user.id)
     refresh_token = create_refresh_token(identity=user.id)
     return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
+
+@app.route("/user-profile", methods=["GET"])
+@jwt_required(refresh=True)
+def user_profile():
+    pass
+
+@app.route("/user-profile/achivements", methods=["GET"])
+@jwt_required(refresh=True)
+def user_achivements():
+    pass
+
+@app.route("/user-profile/orders", methods=["GET"])
+@jwt_required(refresh=True)
+def user_orders():
+    pass
+
+@app.route("/user-profile/tasks", methods=["GET"])
+@jwt_required(refresh=True)
+def user_tasks():
+    pass
+
+@app.route("/user-profile/settings", methods=["GET", "POST"])
+@jwt_required(refresh=True)
+def user_settings():
+    pass
+
+@app.route("/services", methods=["GET"])
+@jwt_required(refresh=True)
+def services():
+    pass
+
+@app.route("/services/gpt", methods=["GET"])
+@jwt_required(refresh=True)
+def gpt():
+    pass
+
+@app.route("/services/mts-services", methods=["GET"])
+@jwt_required(refresh=True)
+def mts():
+    pass
+
+@app.route("/services/yandex-services", methods=["GET"])
+@jwt_required(refresh=True)
+def yandex():
+    pass
+
+@app.route("/services/service-order", methods=["GET"])
+@jwt_required(refresh=True)
+def service_order():
+    pass
+
+@app.route("/services/complaint", methods=["GET"])
+@jwt_required(refresh=True)
+def complaint():
+    pass
+
+@app.route("/services/office-devices", methods=["GET"])
+@jwt_required(refresh=True)
+def office_devices():
+    pass
+
+@app.route("/services/office-map", methods=["GET"])
+@jwt_required(refresh=True)
+def office_map():
+    pass
+
+@app.route("/services/schedule", methods=["GET"])
+@jwt_required(refresh=True)
+def schedule():
+    pass
+
+@app.route("/services/reminders", methods=["GET"])
+@jwt_required(refresh=True)
+def reminders():
+    pass
+
+@app.route('/people-count',  methods=["POST"])
+def people_count():
+    img = request.files['file']
+    url = "http://10.193.159.213:3001/predict"
+    img_data = img.read()
+    response = requests.post(url, files={'file': ('filename.jpg', img_data, 'image/jpeg')})
+    data = response.json()
+    count = data.get('count')
+    return jsonify(count=count)
+
+@app.route('/face-recognition',  methods=["POST"])
+def face_recognition():
+    img = request.files['file']
+    url = "http://10.193.159.213:3002/predict"
+    img_data = img.read()
+    response = requests.post(url, files={'file': ('filename.jpg', img_data, 'image/jpeg')})
+    data = response.json()
+    name = data.get('name')
+    return jsonify(name=name)
