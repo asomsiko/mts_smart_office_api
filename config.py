78a4c9fbe1dcdd6f4f12032ddb62c9e0f854eacd
muz <nadokto8@gmail.com>
2023-10-14 22:28:51 +1000

    chore: улучшение безопасности
    секретныне ключи и данные админ панели теперь берутся из переменных
    окружения

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 class Config(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
-    SECRET_KEY = 'you-will-never-guess'
-    JWT_SECRET_KEY = 'jwt-secret-string'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BASIC_AUTH_USERNAME = 'root'
-    BASIC_AUTH_PASSWORD = 'root'
+    BASIC_AUTH_USERNAME = os.environ.get('ADMIN_USERNAME')
+    BASIC_AUTH_PASSWORD = os.environ.get('ADMIN_PASSWORD')
