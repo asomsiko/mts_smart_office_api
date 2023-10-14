ed4da54a16492348f5c9a1e4f95a5e7432b2a2d2
muz <nadokto8@gmail.com>
2023-10-14 18:22:43 +1000

    feat: cors
    добавлен cors

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ from flask_basicauth import BasicAuth
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 from config import Config
 app = Flask(__name__)
+CORS(app=app)
 app.config.from_object(Config)
 
 basic_auth = BasicAuth(app)
