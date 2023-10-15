4b9c4a7577df8d1a058403394bea21a77cd23efb
muz <nadokto8@gmail.com>
2023-10-15 13:07:03 +1000

    refactor: default config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 class Config(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "you-will-never-guess"
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or "super-secret-key"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BASIC_AUTH_USERNAME = os.environ.get('ADMIN_USERNAME')
-    BASIC_AUTH_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+    BASIC_AUTH_USERNAME = os.environ.get('ADMIN_USERNAME') or "root"
+    BASIC_AUTH_PASSWORD = os.environ.get('ADMIN_PASSWORD') or "root"
